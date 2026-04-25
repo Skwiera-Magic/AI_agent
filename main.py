9a327d230162946845f927c5cfdfa2bb436949e0
skwiera_magic <mckskwiera@gmail.com>
2026-04-25 18:02:17 +0100

    agent responds to aregument requests

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import os
+import argparse
 from dotenv import load_dotenv
-from google import genai 
+from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+    parser = argparse.ArgumentParser(description="Magic's Chatbot feat. Gemini")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     if api_key == None:
         raise RuntimeError("API key did not load <sadface>")
     client = genai.Client(api_key = api_key)
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model='gemini-2.5-flash', contents=messages
     )
     if response.usage_metadata == None:
         raise RuntimeError("Metadata does not exist <sadface>")
