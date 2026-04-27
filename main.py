e689e8046c6b28aee5899d1038729b08b88eede2
skwiera_magic <mckskwiera@gmail.com>
2026-04-27 16:37:19 +0100

    system prompt and call_functions added

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from call_function import available_functions
 
 def main():
     load_dotenv()
@@ -16,7 +18,9 @@ def main():
         raise RuntimeError("API key did not load <sadface>")
     client = genai.Client(api_key = api_key)
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=messages
+        model='gemini-2.5-flash', 
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
     )
     if response.usage_metadata == None:
         raise RuntimeError("Metadata does not exist <sadface>")
@@ -24,7 +28,11 @@ def main():
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(response.text)
+    if response.function_calls is not None:
+        for function in response.function_calls:
+            print(f"Calling function: {function.name}({function.args})")
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
